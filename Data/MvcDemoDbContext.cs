e6a85177c63ce4f0001d90cbc357dabb38512b10
ümit aras <umit.aras96@gmail.com>
2023-06-10 02:47:13 +0300

    Add project files.

---
+++ Data/MvcDemoDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcSqlCrudOpt.Models.Domain;
+
+namespace MvcSqlCrudOpt.Data
+
+{
+    public class MvcDemoDbContext : DbContext
+    {
+        public MvcDemoDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees{ get; set; }
+    }
+}
